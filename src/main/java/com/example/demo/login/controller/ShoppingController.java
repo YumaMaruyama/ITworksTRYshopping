a3eebc8359acecefbc84f90cf656a5d1ab8fd74a
YumaMaruyama <83486993+YumaMaruyama@users.noreply.github.com>
2021-07-29 15:53:46 +0900

    キャンセル機能

--- src/main/java/com/example/demo/login/controller/ShoppingController.java
+++ src/main/java/com/example/demo/login/controller/ShoppingController.java
@@ -692,7 +692,7 @@ public class ShoppingController {
 	@PostMapping(value = "/cancel",params = "confirmation") 
 	public String postCancelConfirmation(@ModelAttribute CancelForm form,@RequestParam("id") int purchaseId ,HttpServletRequest request, HttpServletResponse response,Model model) {
 		
-model.addAttribute("contents", "shopping/cancelConfirmation::productListLayout_contents");
+		model.addAttribute("contents", "shopping/cancelConfirmation::productListLayout_contents");
 		
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		System.out.println("auth" + auth.getName());
@@ -734,6 +734,50 @@ model.addAttribute("contents", "shopping/cancelConfirmation::productListLayout_c
 		session.setAttribute("content",form.getContent());
 		model.addAttribute("content",form.getContent());
 		model.addAttribute("title",(String) session.getAttribute("title"));
+		
+		return "shopping/productListLayout";
+	}
+	
+	@PostMapping(value = "/cancel",params = "detail")
+	public String postCancelDetail(@ModelAttribute CancelForm form,@RequestParam("id") int purchaseId,Model model) {
+		
+model.addAttribute("contents", "shopping/cancelDetail::productListLayout_contents");
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		System.out.println("auth" + auth.getName());
+		String getName = auth.getName();
+		int select_id = usersService.select_id(getName);//メソッドに入ったユーザーのIDを取得
+
+		PurchaseDTO purchasedto = new PurchaseDTO();
+
+		// 購入商品情報取得
+		PurchaseDTO purchasedtoList = purchaseService.reviewSelectHistory(select_id, purchaseId);//Pathで取得した購入IDでpurchaseテーブルの情報を取得
+		purchasedto.setId(purchasedtoList.getId());
+		purchasedto.setPurchaseId(purchasedtoList.getPurchaseId());
+		purchasedto.setPurchase_date(purchasedtoList.getPurchase_date());
+		purchasedto.setPcDataId(purchasedtoList.getPcDataId());
+		purchasedto.setPcName(purchasedtoList.getPcName());
+		purchasedto.setPrice(purchasedtoList.getPrice());
+		purchasedto.setProduct_count(purchasedtoList.getProduct_count());
+		purchasedto.setPurchaseCheck(purchasedtoList.getPurchaseCheck());
+
+		String nullCheck = "null";
+		int getCustomId = customService.selectPurchaseCheck(select_id, purchasedtoList.getProduct_id(), //購入した商品のcustomテーブルIDを取得
+				
+				purchasedtoList.getPurchaseCheck(), nullCheck);
+		System.out.println("getCustomId" + getCustomId);
+
+		PurchaseDTO customList = customService.selectMany(getCustomId);//購入した商品のcustomテーブル情報を取得
+		System.out.println("costomList" + customList);
+
+		purchasedto.setMemory(customList.getMemory());
+		purchasedto.setHardDisc(customList.getHardDisc());
+		purchasedto.setCpu(customList.getCpu());
+		purchasedto.setCustomPrice(customList.getCustomPrice());
+		model.addAttribute("totalPrice", purchasedto.getPrice() + purchasedto.getCustomPrice());
+		model.addAttribute("purchaseId", purchaseId);
+
+		model.addAttribute("purchaseList", purchasedto);
 		
 		return "shopping/productListLayout";
 	}
