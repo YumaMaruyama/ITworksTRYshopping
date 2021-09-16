d8ca972867c69516ccfe84433b891bf754447697
YumaMaruyama <83486993+YumaMaruyama@users.noreply.github.com>
2021-09-16 15:59:37 +0900

    キャンセル機能画面　修正

--- src/main/java/com/example/demo/login/domail/model/CancelForm.java
+++ src/main/java/com/example/demo/login/domail/model/CancelForm.java
@@ -1,6 +1,7 @@
 package com.example.demo.login.domail.model;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
@@ -11,13 +12,13 @@ public class CancelForm {
 
 	private String title;
 	
-	
-	
 	@NotBlank(groups = ValidGroup1.class)
-	@Length(min = 7, max = 7, groups = ValidGroup2.class)
+	@Pattern(regexp = "[0-9]+$",groups = ValidGroup2.class)
+	@Length(min = 7, max = 7, groups = ValidGroup3.class)
 	private String bankNumber;
 	@NotBlank(groups = ValidGroup1.class)
-	@Length(min = 1, max = 3, groups = ValidGroup2.class)
+	@Pattern(regexp = "[0-9]+$",groups = ValidGroup2.class)
+	@Length(min = 1, max = 3, groups = ValidGroup3.class)
 	private String storeName;
 	
 
