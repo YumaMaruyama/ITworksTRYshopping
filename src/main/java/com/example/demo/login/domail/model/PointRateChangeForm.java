55094614a65de56ada86a1bdd3796a3d7b4d9efd
YumaMaruyama <83486993+YumaMaruyama@users.noreply.github.com>
2021-09-17 15:53:12 +0900

    バリデーション　修正

--- src/main/java/com/example/demo/login/domail/model/PointRateChangeForm.java
+++ src/main/java/com/example/demo/login/domail/model/PointRateChangeForm.java
@@ -1,11 +1,18 @@
 package com.example.demo.login.domail.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 
 @Data
 public class PointRateChangeForm {
 
 	private int id;
-	
-	private int pointRate;
+	@NotBlank(groups = ValidGroup1.class)
+	@Pattern(regexp = "[1-9]+$",groups = ValidGroup2.class)
+	@Length(min = 1, max = 1, groups = ValidGroup3.class)
+	private String pointRate;
 }
