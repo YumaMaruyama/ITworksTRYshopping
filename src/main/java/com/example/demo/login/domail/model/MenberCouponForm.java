a5e5633e57c76ec90327d9a9d383febb8d520fb4
YumaMaruyama <83486993+YumaMaruyama@users.noreply.github.com>
2021-09-17 13:43:06 +0900

    バリデーション　修正

--- src/main/java/com/example/demo/login/domail/model/MenberCouponForm.java
+++ src/main/java/com/example/demo/login/domail/model/MenberCouponForm.java
@@ -2,19 +2,25 @@ package com.example.demo.login.domail.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 @Data
 public class MenberCouponForm {
 
-private int id;
+	private int id;
 	
-	private int discount;
-	
-	private int purchaseCountTarget;
-	
-	private int purchaseTotalPriceTarget;
-	
+	@NotBlank(groups = ValidGroup1.class)
+	@Length(min = 1, max = 30, groups = ValidGroup2.class)
 	private String title;
+	
+	@NotBlank(groups = ValidGroup1.class)
+	@Pattern(regexp = "[0-9]+$",groups = ValidGroup2.class)
+	@Length(min = 1, max = 2, groups = ValidGroup3.class)
+	private String discount;
 	
 	private int couponRank;
 	
