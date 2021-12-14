d5a3ebe338e2ac192fe372647611ecab12069c9c
YumaMaruyama <83486993+YumaMaruyama@users.noreply.github.com>
2021-12-14 11:08:13 +0900

    modify
    [productList]
    
    検索フォームバリデーション

--- src/main/java/com/example/demo/login/domail/model/ProductListSearchForm.java
+++ src/main/java/com/example/demo/login/domail/model/ProductListSearchForm.java
@@ -12,15 +12,15 @@ public class ProductListSearchForm {
 	@Length(max = 30, groups = ValidGroup3.class)
 	private String product;
 	
-	@Pattern(regexp = "[a-z]?", groups = ValidGroup2.class)
+	@Pattern(regexp = "[a-zA-Z]*", groups = ValidGroup2.class)
 	@Length(max = 15, groups = ValidGroup3.class)
 	private String os;
 	
-	@Pattern(regexp = "[0-9]+$", groups = ValidGroup2.class)
+	@Pattern(regexp = "[0-9]*", groups = ValidGroup2.class)
 	@Length(max = 9, groups = ValidGroup3.class)
 	private String priceFrom;
 	
-	@Pattern(regexp = "[0-9]+$", groups = ValidGroup2.class)
+	@Pattern(regexp = "[0-9]*", groups = ValidGroup2.class)
 	@Length(max = 9, groups = ValidGroup3.class)
 	private String priceTo;
 }
